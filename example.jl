9ce90075ce3067091dc8f107bc4390fc0a2822f1
Chris <krzysztof.stempinski@queens.ox.ac.uk>
2022-02-07 15:15:40 +0000

    Included an example script for experimental data analysis, and a dedicated raw data loading method with Loess smoothing

---
+++ example.jl
@@ -0,0 +1,33 @@
+"""
+	SIS tools package for julia
+
+	Author: 	Chris Stempinski
+	File:       example.jl
+	
+	TODO description
+"""
+
+push!(LOAD_PATH, "C:/Users/Chris/Projects/SIS/sis")
+include("loadfile.jl")
+using Data, Currents, Recovery, File, Plots
+
+fileName(ω, mm) = string("data/IV_", ω, "GHz_-30mA_", mm, "um.csv")
+
+upVs, upIs = loadFile(fileName("230", "0_00"); Vcol = 2, Icol = 3);
+
+DCₒ1 = parseDCₒData(upVs, upIs);
+
+upVsp, upIsp =  loadFile(fileName("230", "0_40"); Vcol = 2, Icol = 3);
+
+DC1 = parseDCData(upVsp, upIsp, DCₒ1; ν = 230.0)
+
+#=
+# perform impedance recovery=#
+fitVs, fitIs = File.filter(DC1.nVs, DC1.nIs, 2 / DCₒ1.Vg2, 2.54 / DCₒ1.Vg2);
+
+nZLO, nVLO = performRecovery(DCₒ1, DC1, fitVs, fitIs);
+
+Myi(nVₒ) = Ip(DCₒ1, DC1, nVₒ, recover_nVω(DCₒ1, DC1, nVLO, nZLO, nVₒ));
+
+plot(DC1.nVs, DC1.nIs, xlims = (0, 1.3), ylims = (0, 2), linewidth = 1.5, label = "Experimental data", legend = :topleft)
+plot!([0:0.01:2;], Vₒ -> Myi(Vₒ), label = "Recovered I-V from Zemb and VLO" , linewidth = 1.5)
---
+++ loadfile.jl
@@ -0,0 +1,54 @@
+using File, DataFrames, CSV, Loess
+
+"""
+    loadFile(fileName, Vcutoff = 5)
+
+This is a helper method that SHOULD NOT in general be used when performing analysis
+on arbitrary experimental data, as it makes a number of assumptions about the 
+structure of the CSV file. 
+
+"""
+function loadFile(fileName; Vcol = 2, Icol = 3, Vcutoff = 5, smooth = true)
+    
+    df = DataFrame(CSV.File(fileName, delim = ",", limit = 12000, header = false, types=Dict(1=>String, 2=>Float64, 3=>Float64)))
+
+    Vs = df[!, Vcol];
+    Is = df[!, Icol];
+
+    upsweepEnd = upsweepStart = 0;
+    for i in 1:length(Vs)
+        if (Vs[i] > Vcutoff)
+            upsweepEnd = i;
+            break;
+        end
+    end
+
+    for i in length(Vs):-1:1
+        if (Vs[i] < -Vcutoff)
+            upsweepStart = i;
+            break;
+        end
+    end
+
+    @show upsweepStart
+    @show upsweepEnd
+
+    Vs_up = vcat(Vs[upsweepStart:end], Vs[1:upsweepEnd]);
+    Is_up = vcat(Is[upsweepStart:end], Is[1:upsweepEnd]);
+
+    if (smooth)
+
+    # Loess smoothing
+        model = loess(Vs_up, Is_up, span=0.01)
+        us = range(extrema(Vs_up)...; step = 0.01)
+        vs = predict(model, us)
+
+        return [us;], vs ./ 1000 
+
+    else
+
+        return Vs_up, Is_up ./ 1000
+
+    end
+
+end
